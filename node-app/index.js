cf2149a0306b207abb95f12fbf4d5f2e459d6925
UlisesLopez123 <ulises.lopez001.lj@gmail.com>
2025-10-03 16:12:08 -0400

    Update index.js
    Remove line break

--- node-app/index.js
+++ node-app/index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, CI Pipeline!\n');
+  res.end('Hello, CI Pipeline!');
 });
 
 server.listen(8080, () => {
